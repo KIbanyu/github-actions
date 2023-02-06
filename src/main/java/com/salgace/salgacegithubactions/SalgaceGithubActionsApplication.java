d9670fb47e6508de184951451fd75788b76b3758
administrator <mkibanyu96@gmail.com>
2023-02-06 18:47:50 +0200

    initail commit

---
+++ src/main/java/com/salgace/salgacegithubactions/SalgaceGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.salgace.salgacegithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalgaceGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalgaceGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salgace/salgacegithubactions/SalgaceGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salgace.salgacegithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalgaceGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
